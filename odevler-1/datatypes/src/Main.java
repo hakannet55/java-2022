5d108bedb2eda9f3a45f05b0dfe27e1a6a4f0ee5
hakan.kuru <hakan.kuru@etiya.com>
2022-09-28 18:35:49 +0300

    odevler-refactor

--- odevler-1/datatypes/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        double number = 12.2;
-        number = -22;
-        char character = 'A';
-        boolean isItCorrect = false;
-    }
-}
--- odevler-1/variables/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) { /* Psvm short cut */
-        int student = 12;
-        String message = "Öğrenci Sayısı: ";
-
-        System.out.println(message + student);
-        System.out.println("Öğrenci Sayım: " + student);
-    }
-}
