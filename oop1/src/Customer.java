db284424b274b28d5b9113e9a5dec514e7598f65
hakan.kuru <hakan.kuru@etiya.com>
2022-09-28 21:01:35 +0300

    dev-project-update

---
+++ oop1/src/Customer.java
@@ -0,0 +1,29 @@
+public class Customer {
+    private int id;
+    private String custNumber;
+    private String phone;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustNumber() {
+        return custNumber;
+    }
+
+    public void setCustNumber(String custNumber) {
+        this.custNumber = custNumber;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
